cb543f3509d3ca588b739f203d11d8ddc6366b60
TWood <tomwood2@gmail.com>
2016-09-03 17:43:36 -0700

    Add project files.

---
+++ angular2-tour-of-heroes/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+﻿import { Hero } from './hero'
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Chi' },
+    { id: 14, name: 'James' },
+    { id: 15, name: 'Tommy' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+
